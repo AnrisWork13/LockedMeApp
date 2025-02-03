7321ff1ce056855df9171bebd106ab6385877585
Anris Roque Da Costa <dacostaanris@gmail.com>
2025-02-03 10:07:26 -0800

    Fixed input handling, added exit option, and improved error handling

--- src/UI_/LockedMeApp.java
+++ src/UI_/LockedMeApp.java
@@ -14,45 +14,51 @@ public class LockedMeApp {
 		while (attempts < maxAttempts) {
 			System.out.println("\n Choose an Option:");
 			System.out.println("1. List files in asceding order");
-			System.out.println("2. Manage file (Add, Delete, Search");
-			System.out.println("3. Close application");
+			System.out.println("2. Add file");
+			System.out.println("3. delete file");
 			System.out.println("4. Search a file");
+			System.out.println("5. Exit Application");
 			System.out.println("Enter your choice: ");
 			
-			
-			int choice_is_yours = sc.nextInt();
-			
-			if (choice_is_yours == 1) {
-				FileHandling.FileHandler.listFiles();
-			}
-			else if (choice_is_yours == 2) {
-				System.out.println("Enter the filename to be added:");
-				String filenam = sc.next();
-				FileHandling.FileHandler.addFile(filenam);
-			}
-			else if (choice_is_yours==3) {
-				System.out.println(" Enter the filename to be deleted");
-				String filenam = sc.next();
-				FileHandling.FileHandler.deleteFile(filenam);
-				
-			}
-			else if ( choice_is_yours == 4) {
-				System.out.println(" Enter the filename to be searched:");
-				String filenam = sc.next();
-				FileHandling.FileHandler.search_file_data(filenam);
-			}
-			else {
-				attempts++;
-				
-				System.out.println("Invalid Option. Existing the application ");
-				if (attempts == maxAttempts) {
-					System.out.println("Maximum attempts reached, there by existing application");
-					System.exit(0);
-				}
-				
-			}
-			
-		}
-	}
+            int choice = sc.nextInt();
+            sc.nextLine(); 
 
+            switch (choice) {
+                case 1:
+                    FileHandler.listFiles();
+                    break;
+
+                case 2:
+                    System.out.print("Enter the filename to add: ");
+                    String addFile = sc.nextLine();
+                    FileHandler.addFile(addFile);
+                    break;
+
+                case 3:
+                    System.out.print("Enter the filename to delete: ");
+                    String deleteFile = sc.nextLine();
+                    FileHandler.deleteFile(deleteFile);
+                    break;
+
+                case 4:
+                    System.out.print("Enter the filename to search: ");
+                    String searchFile = sc.nextLine();
+                    FileHandler.search_file_data(searchFile);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting the application. Goodbye!");
+                    System.exit(0);
+
+                default:
+                    attempts++;
+                    System.out.println("Invalid option. Please try again.");
+                    if (attempts == maxAttempts) {
+                        System.out.println("Maximum attempts reached. Exiting the application.");
+                        System.exit(0);
+                    }
+                    break;
+            }
+        }
+    }
 }
